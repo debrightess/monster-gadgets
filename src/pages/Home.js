dd4d10eb48d8f815ab209d6e5b4fa52807bd34ca
Bright Tenalo <debrightess@yahoo.com>
2023-02-01 00:36:10 +0100

    home wrapper 1 section update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,90 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner position-relative'>
+                <img
+                  src='images/main-banner-1.jpg'
+                  className='img-fluid rounded-3'
+                  alt='main banner'
+                />
+                <div className='main-banner-content position-absolute'>
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.0</p>
+                  <Link className='button' to='#'>
+                    BUY NOW
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className='col-6'>
+              <div className='d-flex flex-wrap gap-10 justify-content-between align-items-center'>
+                <div className='small-banner position-relative'>
+                  <img
+                    src='images/catbanner-01.jpg'
+                    className='img-fluid rounded-3'
+                    alt='main banner'
+                  />
+                  <div className='small-banner-content position-absolute'>
+                    <h4>BEST SALES</h4>
+                    <h5>Laptops Max</h5>
+                    <p>From $1699.00</p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img
+                    src='images/catbanner-03.jpg'
+                    className='img-fluid rounded-3'
+                    alt='main banner'
+                  />
+                  <div className='small-banner-content position-absolute'>
+                    <h4>NEW ARRIVAL.</h4>
+                    <h5>Buy iPad Air.</h5>
+                    <p>From $599</p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img
+                    src='images/catbanner-02.jpg'
+                    className='img-fluid rounded-3'
+                    alt='main banner'
+                  />
+                  <div className='small-banner-content position-absolute'>
+                    <h4>15% OFF</h4>
+                    <h5>Smartwatch7</h5>
+                    <p>
+                      Shop the latest brand <br /> style and colors.
+                    </p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img
+                    src='images/catbanner-04.jpg'
+                    className='img-fluid rounded-3'
+                    alt='main banner'
+                  />
+                  <div className='small-banner-content position-absolute'>
+                    <h4>FREE ENGRAVING</h4>
+                    <h5>AirPods Max</h5>
+                    <p>
+                      High-fidelity playback & <br /> ultra low distortion
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Home
