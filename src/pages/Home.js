ce095c2ad99af84e1913d890b36ff744a8868896
Bright Tenalo <debrightess@yahoo.com>
2023-02-01 01:20:44 +0100

    update home-wrapper-2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <section className='home-wrapper-1 py-5'>
-        <div className='container-xxl'>
+        <div className='container-xl'>
           <div className='row'>
             <div className='col-6'>
               <div className='main-banner position-relative'>
@@ -76,6 +76,51 @@ const Home = () => {
                     <p>
                       High-fidelity playback & <br /> ultra low distortion
                     </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='home-wrapper-2 py-5'>
+        <div className='container-xl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='services d-flex align-items-center justify-content-between'>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src='images/service.png' alt='services' />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className='mb-0'>From all orders over $100</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src='images/service-02.png' alt='services' />
+                  <div>
+                    <h6>Daily Suprise Offers</h6>
+                    <p className='mb-0'>Save upto 25% off</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src='images/service-03.png' alt='services' />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className='mb-0'>Shop with an expert</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src='images/service-04.png' alt='services' />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className='mb-0'>Get Factory Default Price</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src='images/service-05.png' alt='services' />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className='mb-0'>100% Protected Payment</p>
                   </div>
                 </div>
               </div>
